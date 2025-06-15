bcc555a3c2d2b74fa0f505f3589ab3a73bbf3a44
ᴛɪᴍᴇ ɪs ᴍᴏɴᴇʏ <codexyraa@gmail.com>
2025-06-15 19:27:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from os import getenv
 from dotenv import load_dotenv
 load_dotenv(".env")
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-FORCESUB = getenv("FORCESUB", "")
+API_ID = int(getenv("API_ID", "26544005"))
+API_HASH = getenv("API_HASH", "66f6221e5ce9109827b50eaf3d105025")
+BOT_TOKEN = getenv("BOT_TOKEN", "7564999456:AAGGcrLBHHxrI6Mcmc2Rh_620zKfFuCU0Zg")
+OWNER_ID = int(getenv("OWNER_ID", "5870285414"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://naura:ahahahahaha@naura.al4xuyo.mongodb.net/?retryWrites=true&w=majority&appName=naura")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002554488354"))
+FORCESUB = getenv("FORCESUB", "-1002818541265")
